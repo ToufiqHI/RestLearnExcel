a76f8dbc96acf164934d97a83493c105a6bf56da
ToufiqHI <spare@JAR-LT-102.local>
2025-03-03 17:56:28 +0530

    Excel

--- src/test/java/DataDrivenTest/ExcelDriven.java
+++ src/test/java/DataDrivenTest/ExcelDriven.java
@@ -1,6 +1,47 @@
+//package DataDrivenTest;
+//
+//import __3_Library.AddBook;
+//import io.restassured.RestAssured;
+//import io.restassured.response.Response;
+//import org.apache.http.HttpStatus;
+//import org.testng.Assert;
+//import org.testng.annotations.Test;
+//
+//import java.util.ArrayList;
+//import java.util.HashMap;
+//
+//import static io.restassured.RestAssured.given;
+//
+//public class ExcelDriven
+//{
+//    @Test(description = "test description")
+//    public void postDatXml() throws Exception
+//    {
+//        dataDriven d=new dataDriven();
+//        ArrayList data =d.getData("Addbook","TestData");
+//        System.out.println("Extracted data from Excel: " + data);
+////       HashMap<String,Object> map=new HashMap<>();
+////
+////
+////        map.put("",""); //// Pass key value of body
+//        HashMap<String,Object> map=new HashMap<>();
+//        map.put("name",data.get(0));
+//        map.put("isbn",data.get(1));
+//        map.put("aisle",data.get(2));
+//        map.put("author",data.get(3));
+//
+//        RestAssured.baseURI="http://216.10.245.166/Library/";
+//        Response response= given().header("Content-Type","application/json")
+////                .body(map)
+//                .body(map)
+//                .post("Addbook.php")
+//                .then().assertThat().statusCode(HttpStatus.SC_OK).extract().response();
+//           response.jsonPath().prettyPrint();
+//
+//    }
+//}
 package DataDrivenTest;
 
-import __3_Library.AddBook;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.apache.http.HttpStatus;
@@ -12,31 +53,37 @@ import java.util.HashMap;
 
 import static io.restassured.RestAssured.given;
 
-public class ExcelDriven
-{
-    @Test(description = "test description")
-    public void postDatXml() throws Exception
-    {
-        dataDriven d=new dataDriven();
-        ArrayList data =d.getData("AddProfile","testData");
+public class ExcelDriven {
 
-//        HashMap<String,Object> map=new HashMap<>();
-//        map.put("",""); //// Pass key value of body
-        HashMap<String,Object> map=new HashMap<>();
-        map.put("name",data.get(0));
+    @Test(description = "Post book data from Excel")
+    public void postData() throws Exception {
+        dataDriven d = new dataDriven();
+        ArrayList<String> data = d.getData("Addbook", "TestData");
 
+        if (data.isEmpty()) {
+            throw new IllegalStateException("No data found for the given test case!");
+        }
 
+        System.out.println("Extracted data from Excel: " + data);
 
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("name", data.get(0));
+        map.put("isbn", data.get(1));
+        map.put("aisle", data.get(2));
+        map.put("author", data.get(3));
 
-        RestAssured.baseURI="http://216.10.245.166/Library/";
-        Response response= given().header("Content-Type","application/json")
-//                .body(map)
-                .body("")
+        RestAssured.baseURI = "http://216.10.245.166/Library/";
+        Response response = given()
+                .header("Content-Type", "application/json")
+                .body(map)
                 .post("Addbook.php")
-                .then().assertThat().statusCode(HttpStatus.SC_OK).extract().response();
-        String ID= response.jsonPath().getString("ID");
-        response.jsonPath().prettyPrint();
-        System.out.println(ID);
+                .then()
+                .assertThat().statusCode(HttpStatus.SC_OK)
+                .extract().response();
 
+        response.jsonPath().prettyPrint();
+
+        // Assert response contains "ID"
+        Assert.assertNotNull(response.jsonPath().getString("ID"), "Book ID not generated!");
     }
 }
--- src/test/java/DataDrivenTest/dataDriven.java
+++ src/test/java/DataDrivenTest/dataDriven.java
@@ -1,86 +1,167 @@
+//package DataDrivenTest;
+//import org.apache.poi.ss.usermodel.Cell;
+//import org.apache.poi.ss.usermodel.CellType;
+//import org.apache.poi.ss.usermodel.Row;
+//import org.apache.poi.ss.util.NumberToTextConverter;
+//import org.apache.poi.xssf.usermodel.XSSFSheet;
+//import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+//import java.io.FileInputStream;
+//import java.util.ArrayList;
+//import java.util.Iterator;
+//
+//public class dataDriven
+//{
+//    // identify testcases column by scanning entire first row
+//    // identify test cases which you want
+//    //  Pull  all data from row and feed in to test
+//    /**
+//     * First create object for  workbook it accepts file as FileInputStream so
+//     * Create object for FileInputStream and paste the excel path ..... This is for accessing work book
+//     * Accessing sheet :
+//     *
+//     * @return
+//     */
+//
+//    public ArrayList<String> getData(String testCaseName,String sheetName) throws Exception
+//    {
+//        ArrayList<String> a = new ArrayList<>();
+//        //File input stream arguments
+//        FileInputStream file = new FileInputStream("/Users/spare/Downloads/DemoData.xlsx");
+//        XSSFWorkbook workbook = new XSSFWorkbook(file);
+//        int count = workbook.getNumberOfSheets();
+//        System.out.println(count);
+//        for (int i = 0; i < 3; i++) {
+//            if (workbook.getSheetName(i).equalsIgnoreCase(sheetName)) {
+//                XSSFSheet sheet = workbook.getSheetAt(i);
+//                // identify testcases column by scanning entire first row
+//                Iterator<Row> rows = sheet.iterator();
+//                Row FirstRow = rows.next();
+//                Iterator<Cell> cell = FirstRow.cellIterator();
+//                int k = 0;
+//                int column = 0;
+//                while (cell.hasNext()) {
+//                    Cell val = cell.next();
+//                    if (val.getStringCellValue().equalsIgnoreCase("TestCases")) {
+//                        //desired column
+//                        column = k;
+//                    }
+//                    k++;
+//                }
+//                System.out.println(column);
+//                // identify test cases which you want
+//                while (rows.hasNext())
+//                {
+//                    Row row = rows.next();
+//                    if (row.getCell(column).getStringCellValue().equalsIgnoreCase(testCaseName)) {
+//                        //Pull  all data from row and feed in to test
+//                        Iterator<Cell> ce = row.cellIterator();
+//                        while (ce.hasNext())
+//                        {
+//                          Cell c=ce.next();
+//                          if (c.getCellType()== CellType.STRING)
+//                          {
+//                            a.add(c.getStringCellValue());
+//                           }
+//                          else
+//                          {
+//                              a.add(NumberToTextConverter.toText(c.getNumericCellValue()));
+//                          }
+//                          }
+//
+//                        return a;
+//                       }
+//                }
+//            }
+//        }
+//        return a;
+//    }
+//
+//    public static void main(String[] args)
+//    {
+//
+//    }
+//
+//}
+//
+//
+
 package DataDrivenTest;
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.CellType;
-import org.apache.poi.ss.usermodel.Row;
+
+import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.ss.util.NumberToTextConverter;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 
-public class dataDriven
-{
-    // identify testcases column by scanning entire first row
-    // identify test cases which you want
-    //  Pull  all data from row and feed in to test
-    /**
-     * First create object for  workbook it accepts file as FileInputStream so
-     * Create object for FileInputStream and paste the excel path ..... This is for accessing work book
-     * Accessing sheet :
-     *
-     * @return
-     */
+public class dataDriven {
 
-    public ArrayList<String> getData(String testCaseName,String sheetName) throws Exception
-    {
+    public ArrayList<String> getData(String testCaseName, String sheetName) throws IOException {
         ArrayList<String> a = new ArrayList<>();
-        //File input stream arguments
-        FileInputStream file = new FileInputStream("/Users/spare/Downloads/DemoData.xlsx");
-        XSSFWorkbook workbook = new XSSFWorkbook(file);
-        int count = workbook.getNumberOfSheets();
-        System.out.println(count);
-        for (int i = 0; i < 3; i++) {
-            if (workbook.getSheetName(i).equalsIgnoreCase(sheetName)) {
-                XSSFSheet sheet = workbook.getSheetAt(i);
-                // identify testcases column by scanning entire first row
-                Iterator<Row> rows = sheet.iterator();
-                Row FirstRow = rows.next();
-                Iterator<Cell> cell = FirstRow.cellIterator();
-                int k = 0;
-                int column = 0;
-                while (cell.hasNext()) {
-                    Cell val = cell.next();
-                    if (val.getStringCellValue().equalsIgnoreCase("TestCases")) {
-                        //desired column
-                        column = k;
-                    }
-                    k++;
-                }
-                System.out.println(column);
-                // identify test cases which you want
-                while (rows.hasNext())
-                {
-                    Row row = rows.next();
-                    if (row.getCell(column).getStringCellValue().equalsIgnoreCase(testCaseName)) {
-                        //Pull  all data from row and feed in to test
-                        Iterator<Cell> ce = row.cellIterator();
-                        while (ce.hasNext())
-                        {
-                          Cell c=ce.next();
-                          if (c.getCellType()== CellType.STRING)
-                          {
-                            a.add(c.getStringCellValue());
-                           }
-                          else
-                          {
-                              a.add(NumberToTextConverter.toText(c.getNumericCellValue()));
-                          }
-                          }
 
-                        return a;
-                       }
+        // Use try-with-resources to ensure file is closed properly
+        try (FileInputStream file = new FileInputStream("/Users/spare/Downloads/DemoData.xlsx");
+             XSSFWorkbook workbook = new XSSFWorkbook(file)) {
+
+            int sheetCount = workbook.getNumberOfSheets();
+            for (int i = 0; i < sheetCount; i++) {
+                if (workbook.getSheetName(i).equalsIgnoreCase(sheetName)) {
+                    XSSFSheet sheet = workbook.getSheetAt(i);
+
+                    // Identify test cases column by scanning the first row
+                    Iterator<Row> rows = sheet.iterator();
+                    if (!rows.hasNext()) {
+                        throw new IllegalStateException("Sheet is empty!");
+                    }
+                    Row firstRow = rows.next();
+                    Iterator<Cell> cells = firstRow.cellIterator();
+
+                    int k = 0, column = -1;
+                    while (cells.hasNext()) {
+                        Cell cell = cells.next();
+                        if (cell.getCellType() == CellType.STRING &&
+                                cell.getStringCellValue().equalsIgnoreCase("TestCases")) {
+                            column = k;
+                            break;
+                        }
+                        k++;
+                    }
+
+                    if (column == -1) {
+                        throw new IllegalStateException("'TestCases' column not found!");
+                    }
+
+                    // Find the row with the test case name
+                    while (rows.hasNext()) {
+                        Row row = rows.next();
+                        Cell testCaseCell = row.getCell(column);
+                        if (testCaseCell != null && testCaseCell.getCellType() == CellType.STRING &&
+                                testCaseCell.getStringCellValue().equalsIgnoreCase(testCaseName)) {
+
+                            Iterator<Cell> rowCells = row.cellIterator();
+                            while (rowCells.hasNext()) {
+                                Cell c = rowCells.next();
+
+                                // Skip the first cell (test case name)
+                                if (c.getColumnIndex() == column) {
+                                    continue;
+                                }
+
+                                if (c.getCellType() == CellType.STRING) {
+                                    a.add(c.getStringCellValue());
+                                } else if (c.getCellType() == CellType.NUMERIC) {
+                                    a.add(NumberToTextConverter.toText(c.getNumericCellValue()));
+                                }
+                            }
+                            return a;  // Return the row data once found
+                        }
+                    }
                 }
             }
         }
         return a;
     }
-            public static void main (String[]args)
-            {
-
-            }
-
-
 }
-
 
